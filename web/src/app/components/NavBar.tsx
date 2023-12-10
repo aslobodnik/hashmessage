29443deb11d05937aca7cb0286c74368d3bebc08
aslobodnik <aslobodnik@gmail.com>
2023-12-09 23:03:22 -0500

    create navbar

---
+++ web/src/app/components/NavBar.tsx
@@ -0,0 +1,28 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavBar = () => {
+  const pathname = usePathname();
+  return (
+    <nav className="flex justify-between px-2 py-8 w-full max-w-xl  my-0 mx-auto  relative min-w-[360px]">
+      <div className="flex gap-x-4 font-sans text-lg text-gray-700  font-bold">
+        <Link className={pathname === "/" ? "text-blue-500" : ""} href="/">
+          {" "}
+          Submit
+        </Link>
+        <Link
+          className={pathname === "/records" ? "text-blue-500" : ""}
+          href="/records"
+        >
+          View
+        </Link>
+      </div>
+      <div>
+        <ConnectButton chainStatus="icon" showBalance={false} />
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
