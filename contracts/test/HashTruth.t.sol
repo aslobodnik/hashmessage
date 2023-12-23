128a898488f5733dd2832dd9a10e411db4d7de48
aslobodnik <aslobodnik@gmail.com>
2023-12-23 15:12:50 -0500

    postpone testing

--- contracts/test/HashTruth.t.sol
+++ contracts/test/HashTruth.t.sol
@@ -2,29 +2,14 @@
 pragma solidity ^0.8.19;
 
 import {Test, console2} from "forge-std/Test.sol";
+import "forge-std/Vm.sol";
 import {HashTruth} from "../src/HashTruth.sol";
+import "../lib/openzeppelin-contracts/contracts/utils/Strings.sol";
 
 contract HashTruthTest is Test {
     HashTruth hashTruth;
 
     function setUp() public {
         hashTruth = new HashTruth();
-    }
-
-    function testRevealMsg() public {
-        // Add a record first
-        bytes32 testHash = keccak256(abi.encodePacked("Test Message"));
-        hashTruth.addRecord("signature1", testHash);
-
-        // Reveal the message
-        hashTruth.revealMsg("Test Message", 0);
-
-        // Check if msgRevealor is set correctly
-        (, , , , address msgRevealor, ) = hashTruth.records(0);
-        assertEq(
-            msgRevealor,
-            address(this),
-            "Revealor should be set correctly"
-        );
     }
 }
