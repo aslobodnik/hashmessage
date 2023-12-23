f8fb80a8adf68c3dc57e046b16e40d45692eb97e
aslobodnik <aslobodnik@gmail.com>
2023-12-23 13:49:25 -0500

    store sha256 hash instead of keccak hash

--- contracts/src/HashTruth.sol
+++ contracts/src/HashTruth.sol
@@ -5,11 +5,14 @@ pragma solidity ^0.8.19;
 import {SignatureChecker} from "../lib/openzeppelin-contracts/contracts/utils/cryptography/SignatureChecker.sol";
 import "../lib/openzeppelin-contracts/contracts/utils/Strings.sol";
 
+//TODO: Add events
+//TODO: change msgHash to msgHashSha256
+
 contract HashTruth {
     struct Record {
         uint id;
         string message;
-        bytes32 msgHash; //keccak256("\x19Ethereum Signed Message:\n" + len(message) + message))
+        string msgHashSha256;
         address msgAuthor;
         address msgRevealor;
         bytes msgHashSignature;
@@ -19,14 +22,22 @@ contract HashTruth {
     uint public nextRecordId;
 
     function addRecord(
-        bytes32 _msgHash,
+        string memory _msgHashSha256,
         bytes memory _msgHashSignature
     ) public {
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19Ethereum Signed Message:\n",
+                Strings.toString(bytes(_msgHashSha256).length),
+                _msgHashSha256
+            )
+        );
+
         // Require that the signer is the same as the message sender
         require(
             SignatureChecker.isValidSignatureNow(
                 msg.sender,
-                _msgHash,
+                digest,
                 _msgHashSignature
             ),
             "Invalid signature."
@@ -36,7 +47,7 @@ contract HashTruth {
             Record(
                 nextRecordId,
                 "", // Initially empty message
-                _msgHash,
+                _msgHashSha256,
                 msg.sender, //msgAuthor
                 address(0), //msgRevealor
                 _msgHashSignature
@@ -54,15 +65,11 @@ contract HashTruth {
             record.msgRevealor == address(0),
             "Message has already been revealed."
         );
-        bytes32 _msgHash = keccak256(
-            abi.encodePacked(
-                "\x19Ethereum Signed Message:\n",
-                Strings.toString(bytes(_message).length),
-                _message
-            )
-        );
 
-        if (_msgHash == record.msgHash) {
+        // Convert string to bytes and hash the message using SHA-256
+        string memory _msgHashSha256 = hashString(_message);
+
+        if (Strings.equal(_msgHashSha256, record.msgHashSha256)) {
             record.msgRevealor = msg.sender;
             record.message = _message;
         } else {
@@ -72,5 +79,25 @@ contract HashTruth {
 
     function getRecordsCount() public view returns (uint) {
         return records.length;
+    }
+
+    function hashString(
+        string memory _message
+    ) public pure returns (string memory) {
+        return bytes32ToString(sha256(abi.encodePacked(_message)));
+    }
+
+    function bytes32ToString(
+        bytes32 _bytes
+    ) internal pure returns (string memory) {
+        bytes memory alphabet = "0123456789abcdef";
+        bytes memory str = new bytes(64); // Length of a hex string for bytes32 is 64 characters
+
+        for (uint256 i = 0; i < 32; i++) {
+            str[i * 2] = alphabet[uint8(_bytes[i] >> 4)];
+            str[1 + i * 2] = alphabet[uint8(_bytes[i] & 0x0F)];
+        }
+
+        return string(str);
     }
 }
