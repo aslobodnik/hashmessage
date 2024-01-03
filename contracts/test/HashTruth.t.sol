23f618b84c6b59bffc12c12d8df164a875e1754e
aslobodnik <aslobodnik@gmail.com>
2024-01-03 14:31:34 -0500

    add dupe test

--- contracts/test/HashTruth.t.sol
+++ contracts/test/HashTruth.t.sol
@@ -60,6 +60,19 @@ contract HashTruthTest is Test {
         vm.stopPrank();
     }
 
+    function testFailDupeHashRecord() public {
+        vm.prank(user1);
+        hashTruth.addRecord(
+            "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08",
+            signature
+        );
+        hashTruth.addRecord(
+            "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08",
+            signature
+        );
+        vm.stopPrank();
+    }
+
     function testValidateAddRecord() public {
         vm.prank(user1);
         hashTruth.addRecord(
