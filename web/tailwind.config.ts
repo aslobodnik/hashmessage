f35c9e4d2865d08af1082544e3263bcdbfcdd4ef
aslobodnik <aslobodnik@gmail.com>
2024-01-11 10:49:54 -0300

    add super small font

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -7,6 +7,9 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontSize: {
+      ss: "0.5rem",
+    },
     extend: {
       colors: {
         "custom-blue-gray": "hsl(240, 6%, 63%)", // thorin blue-gray
