ef35d22d7f3b244dbfd9f651b31f68dd21e38bce
aslobodnik <aslobodnik@gmail.com>
2024-01-14 21:33:29 -0500

    add base sepolia

--- web/src/lib/web3.tsx
+++ web/src/lib/web3.tsx
@@ -1,16 +1,16 @@
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 import { configureChains, createConfig } from "wagmi";
-import { base, localhost } from "wagmi/chains";
+import { base, baseSepolia, localhost } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 
-export const chains = [base, localhost];
+export const chains = [base, localhost, baseSepolia];
 
 export const { publicClient, webSocketPublicClient } = configureChains(chains, [
   publicProvider(),
 ]);
 
 const { connectors } = getDefaultWallets({
-  appName: "Hash Name",
+  appName: "Testifi",
   projectId: "d6c989fb5e87a19a4c3c14412d5a7672",
   chains,
 });
