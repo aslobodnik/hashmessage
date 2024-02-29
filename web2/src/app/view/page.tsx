ad005d12ff1c0c0fa407ebef544705ea4a7f7711
aslobodnik <aslobodnik@gmail.com>
2024-02-29 11:16:24 -0700

    add reveal functionality

--- web2/src/app/view/page.tsx
+++ web2/src/app/view/page.tsx
@@ -11,10 +11,12 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
-import { Address } from "viem";
+import { CheckCircledIcon, ReloadIcon } from "@radix-ui/react-icons";
+
+import { Address, sha256, toHex } from "viem";
 
 import { Checkbox } from "@/components/ui/checkbox";
-import { useState, FormEvent } from "react";
+import { useState, FormEvent, useEffect, useMemo } from "react";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -29,7 +31,17 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 
+import { usePonder } from "@/hooks/usePonder";
+import { formatEther } from "viem";
+import { useSimulateContract, useWriteContract } from "wagmi";
+import { localhost } from "wagmi/chains";
+import { testifiAbi } from "@/lib/abi";
+import Link from "next/link";
+
+const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
 type Record = {
+  id: number;
   author: Address;
   hash: string;
   bounty: bigint;
@@ -39,6 +51,7 @@ type Record = {
 export default function View() {
   const [filterBounty, setFilterBounty] = useState(false);
   const [filterRevealed, setFilterRevealed] = useState(false);
+  const ponder = usePonder();
 
   const filterBountyCheck = (value: boolean) => {
     setFilterBounty(value);
@@ -47,12 +60,21 @@ export default function View() {
     setFilterRevealed(value);
   };
 
+  const records: Record[] =
+    ponder?.records?.map((record) => ({
+      author: record.msgAuthor,
+      hash: record.msgHashSha256,
+      bounty: BigInt(record.bounty), // Convert string to bigint
+      message: record.message,
+      id: record.id,
+    })) ?? [];
+
   return (
     <main className="min-h-screen p-6 mx-auto max-w-5xl">
       <NavBar />
       <div className="  max-w-2xl mt-10 mx-auto">
         <h1 className="text-3xl  mb-10  text-gray-600 text-center font-bold">
-          Predictions
+          Statements
         </h1>
         <div className="flex">
           <div className="mt-2 ml-2 flex gap-2">
@@ -73,7 +95,6 @@ export default function View() {
           </div>
         </div>
         <Table>
-          <TableCaption>A list of your recent authors.</TableCaption>
           <TableHeader>
             <TableRow>
               <TableHead className="w-[100px]">Author</TableHead>
@@ -83,15 +104,19 @@ export default function View() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {records.map((record) => (
-              <TableRow key={record.author}>
+            {records.map((record, index) => (
+              <TableRow key={index}>
                 {/*Author*/}
-                <TableCell className="font-medium">{record.author}</TableCell>
+                <TableCell className="font-medium">
+                  {truncateAddress(record.author)}
+                </TableCell>
                 {/*Hash*/}
                 <TableCell>
-                  {" "}
-                  {hashChunks.map((chunk: string, index: number) => (
-                    <div className="font-mono my-1 mx-auto w-fit" key={index}>
+                  {chunkHash(record.hash).map((chunk, chunkIndex) => (
+                    <div
+                      className="font-mono my-1 mx-auto w-fit"
+                      key={chunkIndex}
+                    >
                       {chunk}
                     </div>
                   ))}
@@ -102,7 +127,7 @@ export default function View() {
                 </TableCell>
                 {/*Bounty*/}
                 <TableCell className="text-right">
-                  {record.bounty.toLocaleString()}
+                  {formatEther(record.bounty)}
                 </TableCell>
               </TableRow>
             ))}
@@ -118,6 +143,47 @@ function chunkHash(sha256Msg: string, chunkSize: number = 16): string[] {
 }
 
 function RevealPrediction({ record }: { record: Record }) {
+  const [message, setMessage] = useState("");
+  const [isMatch, setIsMatch] = useState(false);
+  const [hash, setHash] = useState(record.hash);
+
+  const { data, isError, isSuccess, failureReason } = useSimulateContract({
+    abi: testifiAbi,
+    address: CONTRACT_ADDRESS,
+    functionName: "revealAndClaimBounty",
+    args: [message, record.id],
+    chainId: localhost.id,
+  });
+
+  const {
+    data: txHash,
+    writeContract,
+    status: revealRecordStatus,
+    reset: resetWriteContract,
+    isSuccess: revealRecordSuccess,
+  } = useWriteContract();
+
+  useEffect(() => {
+    const computedHash = sha256(toHex(message)).replace(/^0x/, "");
+    if (message.length > 0) {
+      setIsMatch(computedHash === record.hash);
+      setHash(computedHash);
+    } else {
+      setIsMatch(false);
+      setIsMatch(computedHash === record.hash);
+    }
+  }, [message, record.hash]);
+
+  function handleRevealRecord() {
+    writeContract({
+      abi: testifiAbi,
+      address: CONTRACT_ADDRESS,
+      functionName: "revealAndClaimBounty",
+      args: [message, record.id],
+      chainId: localhost.id,
+    });
+  }
+
   return (
     <>
       {record.message === "" ? (
@@ -131,73 +197,101 @@ function RevealPrediction({ record }: { record: Record }) {
             <DialogHeader>
               <DialogTitle>Reveal Message</DialogTitle>
               <DialogDescription>
-                Type in the message to reveal the prediction. If the sha256
-                matches then the button will be enabled.
+                If the sha256 matches then the button will be enabled.
               </DialogDescription>
             </DialogHeader>
 
-            <div className="grow   max-w-lg">
-              <Label className=" text-gray-400">Prediction</Label>
+            <div className="grow max-w-lg">
+              <Label className=" text-gray-400">Statement</Label>
               <Input
                 className=" bg-green-50 mt-1  placeholder:text-gray-400"
                 placeholder="ETH will hit $10,000 by 2030"
+                value={message}
+                onChange={(event) => setMessage(event.target.value)}
               ></Input>
             </div>
 
             <div className="flex gap-2">
-              <div className="w-fit p-2 rounded mt-1 font-mono text-gray-600 bg-green-100">
-                a441b15fe9a3cf5661190a0b93b9decd<br></br>
-                7d04127288cc87256661190a0b93b9de
+              <div className=" w-[324px] h-16 p-2 rounded mt-1 font-mono text-gray-600 bg-green-100">
+                {chunkHash(hash, 32).map((chunk, index) => (
+                  <div key={index}>{chunk}</div>
+                ))}
+              </div>
+              <div className="flex">
+                {isMatch && (
+                  <CheckCircledIcon className="text-green-500  self-center w-10 h-10" />
+                )}
               </div>
             </div>
+
             <DialogFooter>
-              <div className="flex justify-start w-full">
-                <Button type="submit">Reveal</Button>
+              <div className="flex flex-col  justify-start w-full">
+                <div>
+                  <Button
+                    disabled={!isMatch}
+                    onClick={handleRevealRecord}
+                    type="submit"
+                  >
+                    Reveal
+                  </Button>
+                </div>
+                <DisplaySuccessMessage
+                  txSuccess={revealRecordSuccess}
+                  txHash={txHash}
+                />
               </div>
             </DialogFooter>
           </DialogContent>
         </Dialog>
       ) : (
-        <div className="text-center">{record.message}</div>
+        <div className="text-center">{record.message} </div>
       )}
     </>
   );
 }
 
-const records: Record[] = [
-  {
-    author: "0xf39F...2266", // Assuming Address is a string
-    hash: "a441b15fe9a3cf5661190a0b93b9decda441b15fe9a3cf5661190a0b93b9decd",
-    bounty: 250n, // Changed to bigint
-    message: "",
-  },
-  {
-    author: "0xf39F...2261", // Assuming Address is a string
-    hash: "a441b15fe9a3cf5661190a0b93b9decda441b15fe9a3cf5661190a0b93b9decd",
-    bounty: 150n, // Changed to bigint
-    message: "",
-  },
-  {
-    author: "0xf39F...2262", // Assuming Address is a string
-    hash: "a441b15fe9a3cf5661190a0b93b9decda441b15fe9a3cf5661190a0b93b9decd",
-    bounty: 150n, // Changed to bigint
-    message: "",
-  },
-  {
-    author: "0xf39F...2263", // Assuming Address is a string
-    hash: "a441b15fe9a3cf5661190a0b93b9decda441b15fe9a3cf5661190a0b93b9decd",
-    bounty: 150n, // Changed to bigint
-    message: "hello",
-  },
-  {
-    author: "0xf39F...2264", // Assuming Address is a string
-    hash: "a441b15fe9a3cf5661190a0b93b9decda441b15fe9a3cf5661190a0b93b9decd",
-    bounty: 150n, // Changed to bigint
-    message: "",
-  },
-];
+function truncateAddress(address: Address, length: number = 4): string {
+  return `${address.substring(0, length)}...${address.substring(
+    address.length - length
+  )}`;
+}
 
-const hashChunks = chunkHash(
-  "a441b15fe9a3cf5661190a0b93b9decda441b15fe9a3cf5661190a0b93b9decd",
-  16
-);
+function DisplaySuccessMessage({
+  txHash,
+  txSuccess,
+}: {
+  txHash?: string;
+  txSuccess?: boolean;
+}) {
+  function renderTxSuccessMessage() {
+    if (txSuccess) {
+      const url = `https://etherscan.io/tx/${txHash}`;
+      return (
+        <p>
+          {txHash ? (
+            <span>
+              Success! Transaction{" "}
+              <Link
+                className=" underline"
+                href={url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Hash
+              </Link>
+            </span>
+          ) : (
+            "Transaction successful!"
+          )}
+        </p>
+      );
+    }
+    return null;
+  }
+
+  return (
+    <div className="mt-4">
+      <div className="text-blue-500">{renderTxSuccessMessage()}</div>
+    </div>
+  );
+}
